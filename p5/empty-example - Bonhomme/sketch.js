3e93fe44987af6cdb344927283ab83a457fec806
PoppyAL <60897852+PoppyAL@users.noreply.github.com>
2020-03-16 15:16:40 +0100

    Add files via upload

--- p5/empty-example - Bonhomme/sketch.js
+++ p5/empty-example - Bonhomme/sketch.js
@@ -7,32 +7,38 @@ function setup() {
   c2 = color(255,255,255);
   c3 = color(60,60,60);
   c4 = color(140,140,140);
-
-  //noLoop();
 }
 
-let angleCiel = 0;
-let evol = 0.4;
-let evolN = 0.6;
-
 //X Y des nuages :
-let a = 0;
+let aN = 0;
+let bN = 0;
 let b = 0;
 
+//dégradé
 const Y_AXIS = 1;
 const X_AXIS = 2;
 let c1, c2, c3, c4;
+
+//ciel
+let angleCiel = 0;
+let evol = 0.4;
+let evolN = 0.6;
 
 //Robot
 let angleBG = 29;
 let angleBD = -29;
 let angleJG = -29;
 let angleJD = 29;
-let evolBG = 1;
-let evolBD = 1;
-let evolJG = 1;
-let evolJD = 1;
-let reflet = 0;
+let evolBG = 4;
+let evolBD = 4;
+let evolJG = 4;
+let evolJD = 4;
+
+//montagnes
+let mX = 0;
+let mY = -75;
+let evolMX = .334;
+let evolMY = .334;
 
 function draw(){
 
@@ -87,7 +93,6 @@ function draw(){
   point (-240, -300);
   point (-200, -180);
   point (-160, -220);
-  point (-120, -320);
   point (-100, -80);
   point (-70, -150);
   point (-40, -200);
@@ -145,131 +150,174 @@ function draw(){
   arc (100, 300, 75, 75, 200, 290);
   arc (100, 300, 75, 75, -20, 30);
 
+
   pop ();
 
   angleCiel += evol;
 
   //nuages
-
   stroke (225,225,225);
   fill(color(225,225,225))
 
-  ellipse (-1500+a, 300+2.5*b, 40, 40);
-  ellipse (-1530+a, 300+2.5*b, 20, 20);
-  ellipse (-1480+a, 300+2.5*b, 20, 20);
-  ellipse (-1510+a, 300+2.5*b, 30, 30);
+  ellipse (-1500+aN, 300+2.5*bN, 40, 40);
+  ellipse (-1530+aN, 300+2.5*bN, 20, 20);
+  ellipse (-1480+aN, 300+2.5*bN, 20, 20);
+  ellipse (-1510+aN, 300+2.5*bN, 30, 30);
 
-  ellipse (-1000+a, 200-2*b, 40, 40);
-  ellipse (-1030+a, 200-2*b, 20, 20);
-  ellipse (-980+a, 200-2*b, 20, 20);
-  ellipse (-1010+a, 200-2*b, 30, 30);
+  ellipse (-1000+aN, 300-2*bN, 40, 40);
+  ellipse (-1030+aN, 300-2*bN, 20, 20);
+  ellipse (-980+aN, 300-2*bN, 20, 20);
+  ellipse (-1010+aN, 300-2*bN, 30, 30);
   
-  ellipse (-640+a, 300+2*b, 40, 40);
-  ellipse (-600+a, 300+2*b, 20, 20);
-  ellipse (-660+a, 300+2*b, 20, 20);
-  ellipse (-620+a, 300+2*b, 30, 30);
+  ellipse (-640+aN, 300+2*bN, 40, 40);
+  ellipse (-600+aN, 300+2*bN, 20, 20);
+  ellipse (-660+aN, 300+2*bN, 20, 20);
+  ellipse (-620+aN, 300+2*bN, 30, 30);
   
-  ellipse (-540+a, 175-b, 40, 40);
-  ellipse (-500+a, 175-b, 20, 20);
-  ellipse (-560+a, 175-b, 20, 20);
-  ellipse (-520+a, 175-b, 30, 30);
+  ellipse (-540+aN, 175-bN, 40, 40);
+  ellipse (-500+aN, 175-bN, 20, 20);
+  ellipse (-560+aN, 175-bN, 20, 20);
+  ellipse (-520+aN, 175-bN, 30, 30);
 
-  ellipse (-240+a, 175+1.5*b, 40, 40);
-  ellipse (-200+a, 175+1.5*b, 20, 20);
-  ellipse (-260+a, 175+1.5*b, 20, 20);
-  ellipse (-220+a, 175+1.5*b, 30, 30);
+  ellipse (-240+aN, 175+1.5*bN, 40, 40);
+  ellipse (-200+aN, 175+1.5*bN, 20, 20);
+  ellipse (-260+aN, 175+1.5*bN, 20, 20);
+  ellipse (-220+aN, 175+1.5*bN, 30, 30);
 
-  ellipse (-40+a, 220-b, 40, 40);
-  ellipse (-0+a, 220-b, 20, 20);
-  ellipse (-60+a, 220-b, 20, 20);
-  ellipse (-20+a, 220-b, 30, 30);
+  ellipse (-40+aN, 220-bN, 40, 40);
+  ellipse (-0+aN, 220-bN, 20, 20);
+  ellipse (-60+aN, 220-bN, 20, 20);
+  ellipse (-20+aN, 220-bN, 30, 30);
 
-  a += 1;
-  if(a>1955){
-    a = 0;
+  aN += 1;
+  if(aN>1955){
+    aN = 0;
   }
 
-  b += evolN;
-  if(b>30){
+  bN += evolN;
+  if(bN>30){
     evolN = -evolN;
-  } else if (b<0){
+  } else if (bN<0){
     evolN = -evolN;
   }
 
   
   //sol
   stroke(127,127,127);
-  //fill(color(127,127,127));
   setGradient(0, 400, 800, 200, c3, c4, Y_AXIS);
+
+  stroke(60);
+  fill(color(60));
+
+  mX += evolMX;
+  if(mX>75){
+    evolMX = -evolMX
+  } else if (mX<-75){
+    evolMX = -evolMX;
+  }
+
+  mY += evolMY;
+
+  if(mY>75){
+    evolMY = -evolMY
+  } else if (mY<-75){
+    evolMY = -evolMY;
+  }
+
+  if (mY<=0){
+    strokeWeight(0);
+    noFill();
+  } else {
+    strokeWeight(5);
+    fill(color(60));
+  }
+  
+  //ombres qui bougent
+  triangle(50-b, 405, 100-b, 405, 75+mX-b, 405+mY);
+  triangle(200-b, 415, 300-b, 415, 250+mX-b, 415+mY);
+  triangle(300-b, 400, 350-b, 400, 325+mX-b, 400+mY);
+  triangle(400-b, 410, 550-b, 410, 475+mX-b, 410+mY);
+  triangle(600-b, 400, 650-b, 400, 625+mX-b, 400+mY);
+
+  triangle(850-b, 405, 900-b, 405, 875+mX-b, 405+mY);
+  triangle(1000-b, 415, 1100-b, 415, 1050+mX-b, 415+mY);
+  triangle(1100-b, 400, 1150-b, 400, 1125+mX-b, 400+mY);
+  triangle(1200-b, 410, 1350-b, 410, 1275+mX-b, 410+mY);
+  triangle(1400-b, 400, 1450-b, 400, 1425+mX-b, 400+mY);
 
   //montagnes
   stroke(140,140,140);
   strokeWeight(5);
   fill(color(150,150,150));
-  triangle(50,405, 100, 405, 75, 350);
-  triangle(200,415, 300, 415, 250, 300);
-  triangle(300,400, 350, 400, 325, 350);
-  triangle(400,410, 550, 410, 475, 250);
-  triangle(600,400, 650, 400, 625, 350);
+  triangle(50-b, 405, 100-b, 405, 75-b, 350);
+  triangle(200-b, 415, 300-b, 415, 250-b, 300);
+  triangle(300-b, 400, 350-b, 400, 325-b, 350);
+  triangle(400-b, 410, 550-b, 410, 475-b, 250);
+  triangle(600-b, 400, 650-b, 400, 625-b, 350);
+
+  triangle(850-b, 405, 900-b, 405, 875-b, 350);
+  triangle(1000-b, 415, 1100-b, 415, 1050-b, 300);
+  triangle(1100-b, 400, 1150-b, 400, 1125-b, 350);
+  triangle(1200-b, 410, 1350-b, 410, 1275-b, 250);
+  triangle(1400-b, 400, 1450-b, 400, 1425-b, 350);
+
+  b += 1;
+  if(b>795){
+    b = 0;
+  }
 
   //ROBOT
+
   stroke(100,100,100);
   fill(color(230,230,230));
+
   //bras gauche
   push ();
-  translate(390,210);
+  translate(390,260);
   rotate(-angleBG);
   rect(-10,0,20,125);
   pop ();
 
   //jambe gauche
   push ();
-  translate(390,345);
+  translate(390,395);
   rotate(-angleJG);
   rect(-10,0,20,125);
   pop ();
 
   //corps
-
-  rect(350,200,80,150);
-  line (387,150, 387, 100);
-  arc (387, 150, 50, 50,PI, PI);
-  rect(362,150,50,50);
-  line (390, 183, 410, 183);
-  line (393, 165, 393, 170);
+  rect(350,250,80,150);
+  line (387,200, 387, 150);
+  ellipse (387, 200, 50, 50,);
+  rect(362,200,50,50);
+  line (390, 233, 410, 233);
+  line (393, 215, 393, 220);
+  
+  push();
   stroke(color(255,255,255));
-  line (365+reflet, 220, 365+reflet, 330);
-
-  reflet += 0.1;
-  if(reflet>315){
-    reflet -= 0.1;
-  } else if (reflet<365){
-    reflet -= 0.1;
-  }
+  strokeWeight(10);
+  line (365, 270, 365, 380);
+  strokeWeight(7);
+  line (372, 212, 372, 236)
+  pop();
 
   stroke(100,100,100);
+
   //bras droit
   push ();
-  translate(390,210);
+  translate(390,260);
   rotate(-angleBD);
   rect(-10,0,20,125);
   pop ();
 
   //jambes droit
   push ();
-  translate(390, 345);
+  translate(390, 395);
   rotate(-angleJD);
   rect(-10,0,20,125);
-
-
-  
-
-
   pop ();
 
-
   //bras gauche
-
   angleBG -= evolBG;
   if(angleBG>30){
     evolBG = -evolBG;
@@ -278,7 +326,6 @@ function draw(){
   }
 
   //Jambe gauche
-
   angleJG += evolJG;
   if(angleJG>30){
     evolJG = -evolJG;
@@ -287,7 +334,6 @@ function draw(){
   }
   
   //bras droit
-
   angleBD += evolBD;
 
   if(angleBD>30){
@@ -297,7 +343,6 @@ function draw(){
   }
 
   //jambes droite
-
   angleJD -= evolJD;
   console.log(angleJD);
   if(angleJD>30){
@@ -305,5 +350,4 @@ function draw(){
   } else if (angleJD<-30){
     evolJD = -evolJD;
   }
-
 }
